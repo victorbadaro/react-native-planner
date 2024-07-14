52f8c0db3a4ee317f6904ffa98a9c587b5304250
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-07-14 15:35:28 -0300

    feat: add component to show loading states

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,21 @@
+import { Inter_400Regular, Inter_500Medium, Inter_600SemiBold, useFonts } from '@expo-google-fonts/inter';
 import { Slot } from 'expo-router';
 import { StatusBar, View } from 'react-native';
 
+import { Loading } from '@/components/loading';
 import "@/styles/global.css";
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Inter_500Medium,
+    Inter_400Regular,
+    Inter_600SemiBold
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
   return (
     <View className="flex-1 bg-zinc-950">
       <StatusBar
