514df489b9b34e5ff818dd874e6be37fb6d99f63
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-07-14 15:52:30 -0300

    feat: add an input component

---
+++ src/components/input.tsx
@@ -0,0 +1,39 @@
+import { colors } from "@/styles/colors";
+import clsx from "clsx";
+import { ReactNode } from "react";
+import { Platform, TextInput, TextInputProps, View } from "react-native";
+
+type Variants = 'primary' | 'secondary' | 'tertiary';
+
+interface InputProps {
+  children: ReactNode;
+  variant?: Variants;
+}
+
+function Input({ children, variant = 'primary' }: InputProps) {
+  return <View className={clsx('w-full h-16 flex-row items-center gap-2', {
+    'h-14 px-4 rounded-lg border border-zinc-800': variant !== 'primary'
+  }, {
+    'bg-zinc-950': variant !== 'secondary'
+  }, {
+    'bg-zinc-900': variant !== 'tertiary'
+  })}>
+    {children}
+  </View>;
+}
+
+function Field({ ...rest }: TextInputProps) {
+  return (
+    <TextInput
+      className="flex-1 text-zinc-100 text-lg font-regular"
+      placeholderTextColor={colors.zinc[400]}
+      cursorColor={colors.zinc[100]}
+      selectionColor={Platform.OS === 'ios' ? colors.zinc[100] : undefined}
+      {...rest}
+    />
+  );
+}
+
+Input.Field = Field;
+
+export { Input };
