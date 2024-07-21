39a8521d85a45e001a7ff1618f05a3bd59a0f770
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-07-21 17:44:13 -0300

    feat: add basic components to show activities and details from a specific trip

---
+++ src/app/trip/details.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export function Details() {
+  return (
+    <View className="flex-1">
+      <Text className="text-white">Details</Text>
+    </View>
+  );
+}
