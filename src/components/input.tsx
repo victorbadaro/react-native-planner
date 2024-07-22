f16a260e383ae71dfd0fed0a5cdd0a853f7a6435
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-07-22 02:50:39 -0300

    chore: change input component styl

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,23 +1,25 @@
 import { colors } from "@/styles/colors";
 import clsx from "clsx";
 import { ReactNode } from "react";
-import { Platform, TextInput, TextInputProps, View } from "react-native";
+import { Platform, TextInput, TextInputProps, View, ViewProps } from "react-native";
 
 type Variants = 'primary' | 'secondary' | 'tertiary';
 
-interface InputProps {
+interface InputProps extends ViewProps {
   children: ReactNode;
   variant?: Variants;
 }
 
-function Input({ children, variant = 'primary' }: InputProps) {
-  return <View className={clsx('w-full h-16 flex-row items-center gap-2', {
-    'h-14 px-4 rounded-lg border border-zinc-800': variant !== 'primary',
-    'bg-zinc-950': variant === 'secondary',
-    'bg-zinc-900': variant === 'tertiary'
-  })}>
-    {children}
-  </View>;
+function Input({ children, variant = 'primary', className, ...rest }: InputProps) {
+  return (
+    <View className={clsx('min-h-16 max-h-16 flex-row items-center gap-2', {
+      'h-14 px-4 rounded-lg border border-zinc-800': variant !== 'primary',
+      'bg-zinc-950': variant === 'secondary',
+      'bg-zinc-900': variant === 'tertiary'
+    }, className)} {...rest}>
+      {children}
+    </View>
+  );
 }
 
 function Field({ ...rest }: TextInputProps) {
