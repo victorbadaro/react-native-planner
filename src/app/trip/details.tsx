95d9a3a656fc02f047e3c8b596530f8c15517b21
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-07-22 03:59:51 -0300

    feat: add function to list the important links

--- src/app/trip/details.tsx
+++ src/app/trip/details.tsx
@@ -1,12 +1,13 @@
 import { Button } from "@/components/buttons";
 import { Input } from "@/components/input";
 import { Modal } from "@/components/modal";
+import { TripLink, TripLinkProps } from "@/components/tripLink";
 import { linksServer } from "@/server/links-server";
 import { colors } from "@/styles/colors";
 import { validateInput } from "@/utils/validateInput";
 import { Plus } from "lucide-react-native";
-import { useState } from "react";
-import { Alert, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Alert, FlatList, Text, View } from "react-native";
 
 interface DetailsProps {
   tripId: string;
@@ -17,6 +18,7 @@ export function Details({ tripId }: DetailsProps) {
   const [linkTitle, setLinkTitle] = useState('');
   const [linkURL, setLinkURL] = useState('');
   const [isCreatingLinkTrip, setIsCreatingLinkTrip] = useState(false);
+  const [links, setLinks] = useState<TripLinkProps[]>([]);
 
   function resetNewLinkFields() {
     setLinkTitle('');
@@ -51,11 +53,36 @@ export function Details({ tripId }: DetailsProps) {
     }
   }
 
+  async function getTripLinks() {
+    try {
+      const links = await linksServer.getLinksByTripId(tripId);
+
+      setLinks(links);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getTripLinks();
+  }, []);
+
   return (
     <View className="flex-1 mt-10">
       <Text className="text-zinc-50 text-2xl font-semibold mb-2">Links importantes</Text>
 
       <View className="flex-1">
+        {links.length > 0 ? (
+          <FlatList
+            data={links}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => <TripLink data={item} />}
+            contentContainerClassName="gap-4"
+          />
+        ) : (
+          <Text className="text-zinc-400 font-regular text-base mt-2 mb-6">Nenhum link adicionado.</Text>
+        )}
+
         <Button variant="secondary" onPress={() => setShowNewLinkModal(true)}>
           <Plus color={colors.zinc[200]} size={20} />
           <Button.Title>Cadastrar novo link</Button.Title>
