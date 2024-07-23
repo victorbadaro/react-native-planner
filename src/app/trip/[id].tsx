92559388c4d9455920743200b30dff616f0625b9
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-07-23 17:37:55 -0300

    feat: add function to remove a trip from the app storage

--- src/app/trip/[id].tsx
+++ src/app/trip/[id].tsx
@@ -153,6 +153,26 @@ export default function Trip() {
     }
   }
 
+  async function handleRemoveTrip() {
+    try {
+      Alert.alert('Remover viagem', 'Tem certeza que deseja remover a viagem?', [
+        {
+          text: 'Não',
+          style: 'cancel'
+        },
+        {
+          text: 'Sim',
+          onPress: async () => {
+            await tripStorage.remove();
+            router.navigate('/')
+          }
+        }
+      ]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   useEffect(() => {
     getTripDetails();
   }, []);
@@ -224,6 +244,13 @@ export default function Trip() {
           <Button onPress={handleUpdateTrip} isLoading={isUpdatingTrip}>
             <Button.Title>Atualizar</Button.Title>
           </Button>
+
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={handleRemoveTrip}
+          >
+            <Text className="text-red-400 text-center mt-6">Remover viagem</Text>
+          </TouchableOpacity>
         </View>
       </Modal>
 
